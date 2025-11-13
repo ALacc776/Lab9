2956f46f7aa1c97c1a4cc837e83f8ba7201ee19e
Anderson Liew <liew.and@northeastern.edu>
2025-11-13 11:33:12 -0500

    progres

---
+++ src/main/java/decorator.java
@@ -0,0 +1,23 @@
+public class decorator implements PillCounter {
+  @Override
+  public void addPill(int count) {
+
+  }
+
+  @Override
+  public void removePill() {
+
+  }
+
+  @Override
+  public void reset() {
+
+  }
+
+  @Override
+  public int getPillCount() {
+    return 0;
+  }
+
+  // stuck
+}
